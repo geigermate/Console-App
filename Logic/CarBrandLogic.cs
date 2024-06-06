aa93743acd5f8d0587307f7936ec24e0b1c46d5a
geigermate <geigermate@stud.uni-obuda.hu>
2024-06-06 12:13:21 +0200

    Fixed folder paths in sln file

---
+++ Logic/CarBrandLogic.cs
@@ -0,0 +1,68 @@
+﻿using Models;
+using Repository;
+
+namespace Logic
+{
+    public class CarBrandLogic : ICarBrandLogic
+    {
+        IRepository<CarBrand> repository;
+
+        public CarBrandLogic(IRepository<CarBrand> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Create(CarBrand item)
+        {
+            if (item.Name == null)
+            {
+                throw new ArgumentException("Brand name too short!");
+            }
+            else
+            {
+                this.repository.Create(item);
+            }
+
+        }
+
+        public void Delete(int id)
+        {
+            this.repository.Delete(id);
+        }
+
+        public CarBrand Read(int id)
+        {
+            return this.repository.Read(id) ?? throw new ArgumentException("Brand with the specified id doesn't exist");
+        }
+
+        public IQueryable<CarBrand> ReadAll()
+        {
+            return this.repository.ReadAll();
+        }
+
+        public void Update(CarBrand item)
+        {
+            this.repository.Update(item);
+        }
+
+        //NON-CRUD
+
+        public IEnumerable<KeyValuePair<string, int>> BrandWithTheMostModels()
+        {
+            //CarBrand brand = this.repository.ReadAll().Count(b => b.CarModels.Count());
+            var q = from x in this.repository.ReadAll()
+                             orderby x.CarModels.Count() descending
+                             select new KeyValuePair<string, int>(x.Name, x.CarModels.Count());
+            return q.Take(1);
+            //return brand.Name;
+        }
+
+        public IEnumerable<KeyValuePair<string, int>> BrandWithTheLeastModels()
+        {
+            var q = from x in this.repository.ReadAll()
+                    orderby x.CarModels.Count() ascending //növekvő sorrend
+                    select new KeyValuePair<string, int>(x.Name, x.CarModels.Count());
+            return q.Take(1);
+        }
+    }
+}
---
+++ Repository/CarOwnerDbContext.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public partial class CarOwnerDbContext : DbContext
+    {
+        public virtual DbSet<CarBrand> CarBrands { get; set; }
+        public virtual DbSet<CarModel> CarModels { get; set; }
+        public virtual DbSet<Owner> Owners { get; set; }
+
+        public CarOwnerDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public CarOwnerDbContext(DbContextOptions<CarOwnerDbContext> options) : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseInMemoryDatabase("gyak")
+                              .UseLazyLoadingProxies();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CarBrand>(carbrand => carbrand.HasMany(carbrand => carbrand.CarModels)
+                                                              .WithOne(carmodel => carmodel.CarBrand)
+                                                              .HasForeignKey(carmodel => carmodel.CarBrandId)
+                                                              .OnDelete(DeleteBehavior.ClientSetNull));
+
+
+            modelBuilder.Entity<CarModel>(carmodel => carmodel.HasOne(carmodel => carmodel.CarBrand)
+                                                              .WithMany(brand => brand.CarModels)
+                                                              .HasForeignKey(model => model.CarBrandId)
+                                                              .OnDelete(DeleteBehavior.ClientSetNull));
+
+            modelBuilder.Entity<CarModel>(carmodel => carmodel.HasMany(carmodel => carmodel.Owners));
+
+            var brands = new List<CarBrand>()
+            {
+                new CarBrand(){ Id = 1, Name = "Volkswagen"},
+                new CarBrand(){Id=2,Name="Audi" },
+                new CarBrand(){Id=3, Name="Toyota" },
+                new CarBrand(){Id=4, Name="Volvo"}
+            };
+
+            var models = new List<CarModel>()
+            {
+                new CarModel(){ Id=1, Name="Passat", CarBrandId=1, Price=10, OwnerId=1},
+                new CarModel(){Id=2, Name ="A6", CarBrandId=2, Price=15, OwnerId=2},
+                new CarModel(){Id=3, Name="GR Yaris", CarBrandId=3, Price = 20},
+                new CarModel(){Id=4, Name="V70", CarBrandId=4, Price = 5}
+            };
+
+            var owners = new List<Owner>()
+            {
+                new Owner(){Id=1, Name="Máté"},
+                new Owner(){Id=2, Name="Glória"}
+            };
+
+
+            modelBuilder.Entity<CarBrand>().HasData(brands);
+            modelBuilder.Entity<CarModel>().HasData(models);
+            modelBuilder.Entity<Owner>().HasData(owners);
+        }
+    }
+}
